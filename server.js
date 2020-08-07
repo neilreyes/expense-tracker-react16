29fbe37207980ce41111f5f0c1f8514d72ab6172
Rayneil Reyes <neilreyes@users.noreply.github.com>
2020-08-08 03:05:43 +0800

    - Configure mongoose - Setup mongodb

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+const { connect } = require('mongoose');
 
 dotenv.config({path:'./config/config.env'});
 
+connectDB();
+
 const app = express();
 
-app.use('/api/v1/transactions', require('./routes/transactions'));
+app.use("/api/v1/transactions", require("./routes/transactions"));
 
 app.get('/', (req, res)=>{
 	res.send('Hello world')
