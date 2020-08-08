2008d7359261a8539fe9e52d0d783c89e8dec230
Rayneil Reyes <neilreyes@users.noreply.github.com>
2020-08-08 12:46:51 +0800

    Prepare for production

---
+++ client/build/precache-manifest.7543bf91441e285c9b5e172cc6d90eb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55abe06769823eadbb40572f8e5576d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a523729aad8c3f8cf01",
+    "url": "/static/css/main.d820c58d.chunk.css"
+  },
+  {
+    "revision": "10767544f697364937b2",
+    "url": "/static/js/2.62b8e864.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62b8e864.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a523729aad8c3f8cf01",
+    "url": "/static/js/main.ea838eba.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
